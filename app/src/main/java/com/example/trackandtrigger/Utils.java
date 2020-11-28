d275fdd4865a53b8f64921aa82e56e30e4a37aa3
Manasa <manasabollavaram@gmail.com>
2020-11-29 01:42:34 +0530

    Revert "integrated 3 parts"
    This reverts commit dac37dcd

--- app/src/main/java/com/example/trackandtrigger/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.example.trackandtrigger;
-
-public class Utils {
-    // HERE PUT EMAIL AND PASSWORD , LIKE example@gmail.com
-
-    public static final String EMAIL = "coffeeline9@gmail.com";
-    public static final String PASSWORD = "dwcqckylgqngqoxr";
-}
